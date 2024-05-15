46c5def2e11ec080b300d0c72dc68e79e6389677
pochinokk <144725891+pochinokk@users.noreply.github.com>
2024-05-15 16:54:44 +0300

    Add files via upload

---
+++ Java2_practice/src/Practice20/src/main/java/com/example/Practice20/Aspect.java
@@ -0,0 +1,23 @@
+package com.example.Practice20;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Slf4j
+@Component
+@org.aspectj.lang.annotation.Aspect
+public class Aspect {
+    @Before("allServiceMethods()")
+    public void logParameters(JoinPoint joinPoint) {
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        log.info(formatter.format(date));
+    }
+    @Pointcut("within(com.example.Practice20.service.*)")
+    public void allServiceMethods() {}
+}
---
+++ Java2_practice/src/Practice20/src/main/java/com/example/Practice20/entity/Level.java
@@ -0,0 +1,30 @@
+package com.example.Practice20.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "levels")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Level {
+    @Id
+    @Column(name = "id")
+    @SequenceGenerator(name = "levelsIdSeq", sequenceName = "levels_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "levelsIdSeq")
+    private long id;
+    @Column(name = "complexity")
+    private String complexity;
+    @Column(name = "level_name")
+    private String levelName;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "game_id", insertable = false, updatable = false)
+    @JsonIgnore
+    private Game game;
+}
